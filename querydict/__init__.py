331feb1a534ebd0bad3caadf7e485a3a09248bcd
Anshuman Mishra <anshuman852@gmail.com>
2022-05-19 12:44:05 +0530

    upgrade version

--- querydict/__init__.py
+++ querydict/__init__.py
@@ -3,5 +3,5 @@ Exports the current version, other items need to be included manually.
 """
 VERSION_MAJOR = 0
 VERSION_MINOR = 0
-VERSION_PATCH = 1
+VERSION_PATCH = 2
 VERSION_STRING = ".".join(str(i) for i in [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH])
